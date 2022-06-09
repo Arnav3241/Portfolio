8f5d694d9b390a9d8d58b2c7c38a00eaac6c421d
Arnav3241 <105989942+Arnav3241@users.noreply.github.com>
2022-06-09 21:41:14 +0530

    Added OG Meta Tags!

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,19 @@
 import Link from "next/link";
 import Head from 'next/head';
 
+
 const Home = () => {
   return (
-    <div className="Home">
-      <Head> <title> Arnav Singh - Home </title> </Head>
+    <div>
+      <Head> 
+        <title> Arnav Singh -  </title>
+        <meta name='author' content='Arnav Singh' />
+        <meta name='description' content='' />
+        <meta name='keywords' content='' />
+        <meta name='og:title' content='Arnav Singh - Start Learning To Code Right Now!' />
+        <meta name='og:description' content='Hi, I Am Arnav From India Teaching You Code Many Languages like Python, JavaScript And Frameworks Of JS Like React Js, Next Js, Etc.' />
+        <meta name='og:image' content='/logo.png' />
+      </Head>
       <h1>
         Portfolio
       </h1>
