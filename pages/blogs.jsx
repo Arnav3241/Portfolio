253b79ba710d92b97d021aba7f436de09d99e2dc
Arnav3241 <105989942+Arnav3241@users.noreply.github.com>
2022-06-12 16:11:38 +0530

    Improved Navbar!

---
+++ pages/blogs.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const blogs = () => {
+  return (
+    <div>
+      <Head> 
+        <title> Arnav Singh - Blogs </title>
+        <meta name='author' content='Arnav Singh' />
+        <meta name='description' content='' />
+        <meta name='keywords' content='' />
+      </Head>
+      <h1> blogs </h1>
+    </div>
+  );
+};
+
+export default blogs;
