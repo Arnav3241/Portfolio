0af4ab11410540a36742b347c00749886e1ecb8b
Arnav3241 <105989942+Arnav3241@users.noreply.github.com>
2022-06-17 23:42:54 +0530

    Updated Footer!

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <footer className="justify-center text-gray-900 body-font bg-pu">
         <div className="object-center vertCtr container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
           <span className="mt-4 flex title-font font-medium items-center md:justify-start justify-center vertCtr text-gray-900">
-            <Image src="/Logo.png" width={45} height={45} />
+            <Image alt="Logo" src="/Logo.png" width={45} height={45} />
             <span className="ml-3 text-xl">
               Arnav Singh
             </span>
