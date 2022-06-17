d5b5c486d934201270c26b1b2bad4a103580e66f
Arnav3241 <105989942+Arnav3241@users.noreply.github.com>
2022-06-17 20:19:09 +0530

    Fixed Authentication Bigs!

--- contexts/Authentication.js
+++ contexts/Authentication.js
@@ -4,59 +4,14 @@ import { useEffect, useState } from "react";
 import { db, auth } from "./FIrebaseConfig"
 
 
-const logIn = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
-}
-
+// A Authentication Function To Create User via Email and Password
 const signUp = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
 }
 
-// Sign up with email and password
-const signUpViaEmail = async (name, email, password, passwordConfirm, setSuccess, setError, setLoading) => {
-    setLoading(true);
-    if (!email.current.value || !name.current.value || !password.current.value || !passwordConfirm.current.value ) { 
-        setError("Please Enter All The Credentials!"); 
-    }
-    else if (password.current.value !== passwordConfirm.current.value) { 
-        setError("Passwords do not match");
-    }    
-    else { 
-        try { 
-            setError(""); 
-            setSuccess("");
-            const result = await createUserWithEmailAndPassword(auth, email.current.value, password.current.value);
-            const user = result.user;
-            await addDoc(collection(db, "users"), {
-                Uid: user.uid,
-                Name: name.current.value,
-                Auth_Provider: ("Arnav Singh's Portfolio!"),
-                Email: email.current.value,
-                Created_At: String(new Date())
-            });
-            setSuccess("Successfully Created a Account");
-        } catch (error) { 
-            setError(`${error}`) 
-        }
-    }
-    setLoading(false); 
-};
-
-const logInViaEmail = async (email, password, setSuccess, setError, setLoading) => {
-    setLoading(true);
-    if (!email.current.value || !password.current.value) { 
-        setError("Please Enter All The Credentials!"); 
-    }
-    else {
-        try { 
-            await logIn(email.current.value, password.current.value);
-            setSuccess("Successfully Loged In to your Account");
-        } catch (error) {
-            setError(error);
-        }
-    }
-    setLoading(false);
-};
+const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+}
 
 const logOut = () => {
     return signOut(auth);
@@ -81,4 +36,4 @@ const getData = async (uid) => {
     } catch (err) { console.log(err) }
 }
 
-export { useAuth, signUpViaEmail, logIn, logOut, getData, signUp };
+export { useAuth, logIn, logOut, getData, signUp };
