ca41e7ed906893a823151a59f031002c7b39c443
Arnav Singh <105989942+Arnav3241@users.noreply.github.com>
2022-11-06 16:34:56 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ const Document = () => {
       <Head>
         {/*Bootstrap CDN*/} <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+        <meta name="google-site-verification" content="dD2deZHSkAQE36hOaMgGrYuvtBtDiq-78UCVeZllT78" />
       </Head>
       <body  >
         <Main />
